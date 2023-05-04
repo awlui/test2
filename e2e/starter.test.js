a48447584241cea181b7294b71d581b7b9d93ff7
awlui <awlui.inquiries@gmail.com>
2023-05-04 00:22:21 -0700

    d

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -11,6 +11,6 @@ describe('Example', () => {
 
   it('should have welcome screen', async () => {
     // await expect(element(by.id('welcome'))).toBeVisible();
-    expect(true).toBe(true);
+    await expect(element(by.text('Step Onez'))).toBeVisible();
   });
 });
