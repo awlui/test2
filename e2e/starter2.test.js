5c96ebfcce191f0e42aa995d367aef5a4d2ced6e
awlui <awlui.inquiries@gmail.com>
2023-05-05 17:22:32 -0700

    d

---
+++ e2e/starter2.test.js
@@ -0,0 +1,21 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    console.log("RAN?")
+    await device.launchApp();
+    console.log("DONE")
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    // await expect(element(by.id('welcome'))).toBeVisible();
+    await expect(element(by.text('Step One'))).toBeVisible();
+  });
+
+  it('should have welcome screen 2', async () => {
+    // await expect(element(by.id('welcome'))).toBeVisible();
+    await expect(element(by.text('Step Onez'))).not.toBeVisible();
+  });
+});
