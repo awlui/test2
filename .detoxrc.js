7433e02961e5315126b45335e270ea9ce9b67128
awlui <awlui.inquiries@gmail.com>
2023-05-09 13:54:24 -0700

    change binary path

--- .detoxrc.js
+++ .detoxrc.js
@@ -30,7 +30,7 @@ module.exports = {
     },
     'android.release': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release-androidTest.apk',
       build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
     }
   },
